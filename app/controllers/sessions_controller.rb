c7f605d69ee4c8809d799f7f7f0159eb81660ab2
Taisei-rgb <taisei.contact@gmail.com>
2020-12-15 13:01:36 +0900

    flashメッセージの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     
-    else  
+    else 
+      flash.now[:danger] = "Invalid email/password combination"
       render "new"
     end
   end
